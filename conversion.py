ec7ce2fae9ba46dcb70a317f066bba5c2cef224b
Amanda Henning <anhenning18@gmail.com>
2016-10-14 10:08:06 -0400

    added rest of the conversion functions

--- conversion.py
+++ conversion.py
@@ -6,4 +6,16 @@ def gallons2liters(gallons):
 	liters = gallons * 3.78541
 	return liters
 
+def hours2minutes(hours):
+	minutes = hours * 60
+	return minutes
+
+def feet2inches(feet):
+	inches = feet * 12
+	return inches
+
+def degrees2radians(degrees):
+	radians = degrees * 0.0174533
+	return radians
+
 
